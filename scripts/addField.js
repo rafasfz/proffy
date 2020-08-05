2b4c608200e25e9b45764497a0f0fc17d66122f1
Rafael Freire <rafaelsilvafreire2@gmail.com>
2020-08-05 19:22:31 -0300

    added trash button

--- scripts/addField.js
+++ scripts/addField.js
@@ -2,7 +2,27 @@
 const cloneField = () => {
     // Duplicar os campos
     const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true);
+    
+    const divTrash = document.createElement('div');
+    divTrash.setAttribute('class', 'block-trash');
 
+    const trash = document.createElement('button');
+    trash.setAttribute('id', 'trash');
+    trash.setAttribute('type', 'button');
+
+    const trashIcon = document.createElement('img');
+    trashIcon.setAttribute('src', '/images/icons/trash.png');
+    trashIcon.setAttribute('width', '32px');
+    trashIcon.setAttribute('height', '32px');
+
+    trash.appendChild(trashIcon);
+    divTrash.appendChild(trash);
+
+    trash.addEventListener('click', () => {
+        newFieldContainer.remove();
+    });
+
+    newFieldContainer.appendChild(divTrash);
     // limpar os campos
     const fields = newFieldContainer.querySelectorAll('input');
     fields.forEach((field) => {
